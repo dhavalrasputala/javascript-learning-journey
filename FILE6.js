c4529e691423bacbbad713e751348549f15e614f
Dhaval Rasputala <dhavalrasputala@gmail.com>
2026-01-11 11:10:20 +0530

    Exercises given at end of Chapters

--- FILE6.js
+++ FILE6.js
@@ -109,3 +109,31 @@ function countChar(examplestring,lttr){
 }
 console.log(`the Number of ${lttr} in ${examplestring}is:`,countChar(examplestring,lttr))
 //the Number of Y in BoBBYYYYYYYYYis: 9
+
+/*Write a range function that takes two arguments, start and end, and returns
+an array containing all the numbers from start up to (and including) end.
+Next, write a sum function that takes an array of numbers and returns the
+sum of these numbers. Run the example program and see whether it does
+indeed return 55.
+As a bonus assignment, modify your range function to take an optional third
+argument that indicates the “step” value used when building the array. If no
+step is given, the elements go up by increments of one, corresponding to the
+old behavior. The function call range(1, 10, 2) should return [1, 3, 5, 7,
+9]. Make sure it also works with negative step values so that range(5, 2,-1)
+produces [5, 4, 3, 2].*/
+let str = 10,end=20
+function range(str,end){
+    arr=[]
+    for(let i =str;i<end;i++){
+        arr.push(i)
+    }
+    return arr
+}
+function sum(number){
+    let total=0
+    for(let num of number){
+        total += num
+    }
+    return total
+}
+console.log(sum(range(str,end)))
