87c4f0a0cd7e0d2a91e77294a05b326f41dbb78f
Dhaval Rasputala <dhavalrasputala@gmail.com>
2026-01-11 10:51:56 +0530

    Exercises given at end of Chapters

--- FILE6.js
+++ FILE6.js
@@ -73,3 +73,39 @@ function EvenOdd(num){
     }
 }
 console.log(EvenOdd(50))
+/*You can get the Nth character, or letter, from a string by writing "string"[N].
+The returned value will be a string containing only one character (for example,
+"b"). The first character has position 0, which causes the last one to be found at
+position string.length- 1. In other words, a two-character string has length
+2, and its characters have positions 0 and 1.
+Write a function countBs that takes a string as its only argument and returns
+a number that indicates how many uppercase “B” characters there are in the
+string.
+Next, write a function called countChar that behaves like countBs, except
+it takes a second argument that indicates the character that is to be counted
+(rather than counting only uppercase “B” characters). Rewrite countBs to
+make use of this new function.*/
+let examplestring="BoBBYYYYYYYYY"
+let lttr='Y'
+function countBs(examplestring){
+    let count = 0
+    for (let i = 0;i<examplestring.length;i++){
+        if(examplestring[i]=='B'){
+            count+=1
+        }
+    }
+    return count
+}
+console.log(`the number of "B's"in ${examplestring}is:`,countBs(examplestring))
+//the number of "B's"in BoBBYYYYYYYYYis: 3
+function countChar(examplestring,lttr){
+    let count2=0
+    for (let i = 0 ;i<examplestring.length;i++){
+        if(examplestring[i]==lttr){
+            count2+=1
+        }
+    }
+    return count2
+}
+console.log(`the Number of ${lttr} in ${examplestring}is:`,countChar(examplestring,lttr))
+//the Number of Y in BoBBYYYYYYYYYis: 9
