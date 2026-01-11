7976c27ba4020de1ab362c326ab52ce8522082df
Dhaval Rasputala <dhavalrasputala@gmail.com>
2026-01-11 10:09:17 +0530

    Exercises given at end of Chapters

---
+++ FILE6.js
@@ -0,0 +1,56 @@
+//####All the Exercises given after each Chapter####
+//1st print the triangle
+/*#
+##
+###
+####
+#####
+######
+#######*/
+//code:
+for (let i = 0;i<=7;i++){
+    line = ''
+    for(let j=0;j<=i+1;j++){
+        line +='#'
+    }
+    console.log(line)
+}
+
+/*Write a program that uses console.log to print all the numbers from 1 to 100,
+with two exceptions. For numbers divisible by 3, print "Fizz" instead of the
+number, and for numbers divisible by 5 (and not 3), print "Buzz" instead.
+When you have that working, modify your program to print "FizzBuzz" for
+numbers that are divisible by both 3 and 5 (and still print "Fizz" or "Buzz"
+for numbers divisible by only one of those).*/
+for (let i = 1; i <= 100; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("FizzBuzz");
+  } else if (i % 3 === 0) {
+    console.log("Buzz");
+  } else if (i % 5 === 0) {
+    console.log("Fizz");
+  } else {
+    console.log(i);
+  }
+}
+/*Write a program that creates a string that represents an 8×8 grid, using newline
+characters to separate lines. At each position of the grid there is either a space
+or a "#" character. The characters should form a chessboard.
+Passing this string to console.log should show something like this:
+# # # #
+ # # # #
+# # # #
+ # # # #
+# # # #
+ # # # #
+# # # #
+ # # # #*/
+let string = '#'
+for(let i = 0;i<8;i++){
+    line=''
+    for(let j = 0;j<8;j++){
+        line+=(i+j)%2===0?'#':' '
+    }
+    line +='\n'
+    console.log(line)
+}
